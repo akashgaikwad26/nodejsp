a43edddc178f53d1ad09058948c34dd0ce5f74e5
akashgaikwad26 <gaikwadak172@gmail.com>
2024-11-15 15:54:34 +0530

    changes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,8 @@
 # tests/test_calculator.py
 
 import pytest
-from app.calculator import add, subtract, multiply, divide
+from Routes.calculator import add,subtract,multiply,divide
+# from app.calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
